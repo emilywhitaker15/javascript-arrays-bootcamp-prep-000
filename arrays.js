c605ffa3876aefdf76ab471e35340319edb28c09
emilywhitaker15@gmail.com <emilywhitaker15@gmail.com>
2018-05-16 15:55:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,3 +13,12 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array = array.unshift(element)
   return array
 }
+
+function adElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array = array.push(element)
+  return array
+}
